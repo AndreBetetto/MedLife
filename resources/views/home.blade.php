72639a9329ce7a07b143971b1cd515240ca45337
Miguel de Oliveira Correia <ra2157051@CTIBAURU.FEB.UNESP.BR>
2023-08-10 10:47:38 -0300

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <!-- header -->
-    <main>
-        <section class="w-40 bg-green-400">
-            <div class="">
+<x-guest-layout>
 
-            </div>
-        </section>
-    </main>
-</body>
-</html>
+    <p class="my-4 text-red-500">teste</p>
+
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
