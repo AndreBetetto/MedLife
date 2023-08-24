b755d1b9551bd637be82824231ad6656d22f26b1
eiidm <luisa.eid@unesp.br>
2023-08-24 13:29:40 -0300

    update: forms_sintomas

--- resources/views/medico/forms_diario/indexCriarForm.blade.php
+++ resources/views/medico/forms_diario/indexCriarForm.blade.php
@@ -1,9 +1,4 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            Criar forms
-        </h2>
-    </x-slot>
+
 
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -14,4 +9,3 @@
             </div>
         </div>
     </div>
-</x-app-layout>
--- resources/views/medico/forms_diario/show.blade.php
+++ resources/views/medico/forms_diario/show.blade.php
@@ -1,12 +1,14 @@
+@include('layouts.head')
+@include('layouts.header')
 <div>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <h2>Informações Pós-Operatório</h2>
-    <br>
+    <h2 class="text-xl font-semibold leading-tight text-gray-800">
+        Informações Pós-Operatório
+    </h2><br>
 
-    <form class="w-full max-w-lg">
-        <div class="flex flex-wrap -mx-3 mb-6">
-
-        <div>
+    <form class="w-full">
+        <div class="grid grid-cols-2 items-center  w-full -mx-3 mb-6">
+        <div class="w-full flex flex-col items-center">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Médico
@@ -43,7 +45,7 @@
             </div>
         </div>
         
-        <div>
+        <div class="w-full flex flex-col items-center">
             <div class="w-full md:w-max px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                     Dia
@@ -56,44 +58,44 @@
                     Grau de dor                    
                 </label>
                     <div class="inline-flex rounded-md shadow-sm" role="group">
-                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-verde-1 border border-gray-200 rounded-l-lg hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                            <i class="material-icons">flash_on</i>
+                        <button type="button" title="aaa" class="px-3 py-1 text-sm font-medium text-gray-900 bg-verde-1 border border-black rounded-l-lg focus:z-10 focus:ring-2 focus:ring-black focus:text-grey-7000 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            1
                         </button>
 
-                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-verde-2 border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                            <i class="material-icons">flash_on</i>
+                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-verde-2 border border-black focus:z-10 focus:ring-2 focus:ring-black focus:text-grey-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            2
                         </button>
 
-                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-verde-3 border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                            <i class="material-icons">flash_on</i>
+                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-verde-3 border border-black focus:z-10 focus:ring-2 focus:ring-black focus:text-grey-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            3
                         </button>
 
-                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-amarelo-1 border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                            <i class="material-icons">flash_on</i>
+                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-amarelo-1 border border-black focus:z-10 focus:ring-2 focus:ring-black focus:text-grey-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            4
                         </button>
 
-                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-amarelo-2 border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                            <i class="material-icons">flash_on</i>
+                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-amarelo-2 border border-black focus:z-10 focus:ring-2 focus:ring-black focus:text-grey-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            5
                         </button>
 
-                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-laranja-1 border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                            <i class="material-icons">flash_on</i>
+                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-laranja-1 border border-black focus:z-10 focus:ring-2 focus:ring-black focus:text-grey-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            6
                         </button>
 
-                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-laranja-2 border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                            <i class="material-icons">flash_on</i>
+                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-laranja-2 border border-black focus:z-10 focus:ring-2 focus:ring-black focus:text-grey-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            7
                         </button>
 
-                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-vermelho-1 border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                            <i class="material-icons">flash_on</i>
+                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-vermelho-1 border border-black focus:z-10 focus:ring-2 focus:ring-black focus:text-grey-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            8
                         </button>
 
-                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-vermelho-2 border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                            <i class="material-icons">flash_on</i>
+                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-vermelho-2 border border-black focus:z-10 focus:ring-2 focus:ring-black focus:text-grey-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            9
                         </button>
 
-                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-vermelho-3 border border-gray-200 rounded-r-md hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                            <i class="material-icons">flash_on</i>
+                        <button type="button" class="px-3 py-1 text-sm font-medium text-gray-900 bg-vermelho-3 border border-black rounded-r-md focus:z-10 focus:ring-2 focus:ring-black focus:text-grey-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                            10
                         </button>
                     </div>
             </div><br>
@@ -102,12 +104,31 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                     Grau de Sangramento
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" disabled id="grid-last-name" type="text">
+                
+                <label>
+                    <input class="with-gap" name="group1" type="radio" />
+                    <span>Nenhum</span>
+                </label><br>
+
+                <label>
+                    <input class="with-gap" name="group1" type="radio"  />
+                    <span>Pouco</span>
+                </label><br>
+
+                <label>
+                    <input class="with-gap" name="group1" type="radio"  />
+                    <span>Médio</span>
+                </label><br>
+
+                <label>
+                    <input class="with-gap" name="group1" type="radio"  />
+                    <span>Intenso</span>
+                </label><br>
             </div>
 
             <div class="w-full md:w-max px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                    Febre
+                    <br><br>Febre
                     <div id="test-slider">
                         <input type="range" min="0" max="100" />
                     </div>
@@ -118,7 +139,42 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                     Outros sintomas
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" disabled id="grid-last-name" type="text">
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" maxlength="100">
+            </div>
+        </div> 
+
+        <hr class="mx-4 border-gray-300">
+        <hr class="border-gray-300">
+
+        <div class="flex flex-wrap -mx-3 mb-6 items-center">
+            <div class="w-full px-3 text-center">
+                <h2 class="text-xl font-semibold leading-tight text-gray-800 items-center">
+                    <br>Medicamentos
+                </h2><br>
+            </div>
+            
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 px-10" for="grid-last-name">
+                Dipirona&nbsp; &nbsp;
+                <label>
+                    <input type="checkbox" class=""/>
+                    <span>08h</span> &nbsp; &nbsp;
+
+                    <input type="checkbox" />
+                    <span>10h</span>&nbsp; &nbsp;
+
+                    <input type="checkbox" />
+                    <span>12h</span>&nbsp; &nbsp;
+                </label>
+            </label>
+
+           
+        </div>
+
+        <div class="flex flex-wrap -mx-3 mb-6 justify-center items-center">
+            <div class="w-full px-3 text-center">
+                <h2 class="text-xl font-semibold leading-tight text-gray-800 items-center">
+                    <br>Curativos
+                </h2><br>
             </div>
         </div>
     </div>
