dd5c816b1a7f6bbefd559dfddd4b271c365007e9
naydhow <naydhow@hotmail.com>
2023-08-24 18:50:51 -0300

    feat: theme-switcher

--- resources/js/themeSwitcher.js
+++ resources/js/themeSwitcher.js
@@ -1,17 +1,29 @@
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark')
+  document.documentElement.classList.add("dark");
+  document.querySelector("#theme-switcher").setAttribute("data-theme", "light");
+  document.querySelector("#theme-switcher-text").textContent="Light";
+  document.querySelector("#theme-switcher-icon").setAttribute("src", "sun.png");
 } else {
-  document.documentElement.classList.remove('dark')
+  document.documentElement.classList.remove("dark");
+  document.querySelector("#theme-switcher").setAttribute("data-theme", "dark");
+  document.querySelector("#theme-switcher-text").textContent="Dark";
+  document.querySelector("#theme-switcher-icon").setAttribute("src", "moon.png");
 }
 
 function setDarkTheme() {
   document.documentElement.classList.add("dark");
   localStorage.theme = "dark";
+  document.querySelector("#theme-switcher").setAttribute("data-theme", "light");
+  document.querySelector("#theme-switcher-text").textContent="Light";
+  document.querySelector("#theme-switcher-icon").setAttribute("src", "sun.png");
 };
 
 function setLightTheme() {
   document.documentElement.classList.remove("dark");
   localStorage.theme = "light";
+  document.querySelector("#theme-switcher").setAttribute("data-theme", "dark");
+  document.querySelector("#theme-switcher-text").textContent="Dark";
+  document.querySelector("#theme-switcher-icon").setAttribute("src", "moon.png");
 };
 
 function onThemeSwitcherItemClick(event) {
