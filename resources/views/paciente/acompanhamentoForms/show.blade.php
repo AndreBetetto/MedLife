c481fb89f5d5e5095c35e3add6ecb6e8e39bf7d9
eiidm <luisa.eid@unesp.br>
2023-09-21 10:40:25 -0300

    feat:update telas paciente

--- resources/views/paciente/acompanhamentoForms/show.blade.php
+++ resources/views/paciente/acompanhamentoForms/show.blade.php
@@ -1,6 +1,9 @@
 <div>
     {{-- The whole world belongs to you. --}}
-    Acompanhar resultados de formularios
+    <div class="px-4 sm:px-0">
+        <h3 class="text-xl font-semibold leading-7 text-gray-900 py-3">Resultados dos formulários</h3>
+    </div>
+  
 
     Dia selecionado:
 
