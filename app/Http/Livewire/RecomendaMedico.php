d3bc6ef190b92cf38442ea51fb8e2903ce2fb28d
AndreBetetto <andre.betetto@unesp.br>
2023-09-24 14:13:27 -0300

    feat:update-doctor-search

--- app/Http/Livewire/RecomendaMedico.php
+++ app/Http/Livewire/RecomendaMedico.php
@@ -3,16 +3,131 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
+use log;
+use App\Models\Paciente;
+use Illuminate\Support\Facades\Auth;
 
 class RecomendaMedico extends Component
 {
-    public $medico = 'a';
     public $input;
+    public $output;
+    public $symptoms = [];
+    public $symptomsSelected = [];
+    public $sym = [];
+    public $dataFetched = false;
+    public $saida = [];
 
     public function render()
     {
         return view('livewire.recomenda-medico');
     }
+
+    public function recomenda()
+    {
+        $this->getSpecialization();
+    }
+
+    public function submitSelectedSymptoms()
+    {
+        //transform array in srting
+        $this->symptomsSelected = implode(",", $this->symptomsSelected);
+        //format string in []
+        $this->symptomsSelected = "[" . $this->symptomsSelected . "]";
+        //dd($this->symptomsSelected);
+        return $this->symptomsSelected;
+    }
+
+    public function getSpecialization()
+    {
+        $symptoms = $this->submitSelectedSymptoms();
+        $sexo = $this->getSexo();
+        $idade = $this->getIdade();
+        $token = $this->getToken();
+        $response = Http::get('https://sandbox-healthservice.priaid.ch/diagnosis/specialisations', [
+            'symptoms' => $symptoms,
+            'gender' => $sexo,
+            'year_of_birth' => $idade,
+            'token' => $token,
+            'language' => 'en-gb',
+        ]);
+        if ($response->successful()) {
+            $this->saida = $response->json();
+        } else {
+            // Handle the API request failure
+            $this->saida = [];
+            // You can log an error message or set a default value for $this->symptoms
+        }
+        //dd($response);
+    }
+
+    public function getSexo()
+    {
+        $paciente = Paciente::where('user_id', Auth::user()->id)->first();
+        $outSexo = null;
+        $sexo = $paciente->sexo;
+        if ($sexo == 'Masc') {
+            $outSexo = 'male';
+            
+        } else {
+            $outSexo = 'female';
+        }
+        return $outSexo;
+    }
+
+    public function getIdade()
+    {
+        $paciente = Paciente::where('user_id', Auth::user()->id)->first();
+        //get birth year of patient
+        $idade = Carbon::parse($paciente->dataNasc)->format('Y');
+        return $idade;
+    }
+
+    public function getTokenRead()
+    {
+        $token = env('HuggingFace_read');
+        return $token;
+    }
+
+    public function getTokenWrite()
+    {
+        $token = env('HuggingFace_write');
+        return $token;
+    }
+
+    public function getToken()
+    {
+        $token = env('APIMEDIC_SAND_KEY');
+        return $token;
+    }
+
+    public function getSymptoms()
+    {
+        $token =$this->getToken();
+        $response = Http::get('https://sandbox-healthservice.priaid.ch/symptoms', [
+            'token' => $token,
+            'language' => 'en-gb',
+        ]);
+        if ($response->successful()) {
+            $this->symptoms = $response->json();
+        } else {
+            // Handle the API request failure
+            $this->symptoms = [];
+            // You can log an error message or set a default value for $this->symptoms
+        }
+    }
+
+    public function fetchAPIdata()
+    {
+        // Set isLoading to true to show the loading spinner 
+        // Perform your API requests here
+        $this->getSymptoms();
+        // Set isLoading to false when the API request is completed
+        $this->dataFetched = true;
+    }
+
 
     public function recomendaMedico()
     {
@@ -20,7 +135,22 @@ class RecomendaMedico extends Component
         $preparo = "Retorne em uma palavra a especialidade medica recomendada para consulta, baseado nos seguintes sintomas: ";
         $pos = ". Caso a resposta nao esteja relacionada com sintomas, digite 'erro! Tente novamente'";
         $entrada = $preparo . $this->input . $pos;
+        $ch = curl_init();
 
-       
+        curl_setopt($ch, CURLOPT_URL, 'https://api-inference.huggingface.co/models/gpt2');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, '{"inputs": "'.$entrada.'"}');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Authorization: Bearer '.$this->getTokenRead(),
+        ));
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+
+        $this->output = $result;
+
     }
 }
--- resources/views/livewire/recomenda-medico.blade.php
+++ resources/views/livewire/recomenda-medico.blade.php
@@ -1,13 +1,26 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-
-    <div>
-        <form wire:submit.prevent="recomendaMedico">
-            <input type="text" wire:model="input">
+    <button wire:click.prevent="fetchAPIdata">Fetch Data</button>
+    <div>   
+        <form wire:submit.prevent="recomenda">
+            @if ($dataFetched == true)
+                <select id="symptomsSelected[]" wire:model="symptomsSelected" multiple >
+                    @foreach ($symptoms as $symptom)
+                        <option value={{ $symptom['ID'] }}>{{ $symptom['Name'] }}-{{ $symptom['ID'] }}</option>
+                    @endforeach
+                </select>
+            @endif
+            <br><br><br>
             <button type="submit">Submit</button>
-        </form>
-    
-        <p>{{ $medico }}</p>
+        </form> 
+        Resultado:
+        @forelse ($saida as $out)
+            {{ $out['ID']}} - {{ $out['Name']}} - {{ $out['Accuracy']}}%
+            <br>
+        @empty
+            Clinico geral
+        @endforelse
         <br><br><br>
+        
     </div>
 </div>
