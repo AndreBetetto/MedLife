b79c13d43a8d8d80568d71e33527bb2bdf11ed42
Naydhow Roberto Mascareli Bertaglia <ra2157052@CTIBAURU.FEB.UNESP.BR>
2023-09-26 15:15:32 -0300

    feat: tela arrumada

--- resources/views/livewire/busca-nome-remedio.blade.php
+++ resources/views/livewire/busca-nome-remedio.blade.php
@@ -6,16 +6,16 @@
 
             <div class="form-group dark:bg-slate-800 grid gap-4">
                 <div class="grid gap-2">
-                    <input type="hidden" name='paciente_id' class="dark:bg-slate-800" value="{{ $row->id }}">
-                    <input type="hidden" name="medico_id" class="dark:bg-slate-800" value="{{$medico->id}}">
+                    <input type="hidden" name='paciente_id' value="{{ $row->id }}">
+                    <input type="hidden" name="medico_id" value="{{$medico->id}}">
                     <div class="grid gap-1">
                         <span>Número de dias:</span>
-                        <input type="number" name="numDias" class="dark:bg-slate-800" value="7">
+                        <input type="number" name="numDias" class="dark:bg-slate-800 relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 sm:text-sm sm:leading-6" value="7">
                         <x-input-error class="mt-2" :messages="$errors->get('numDias')" />
                     </div>
                     <div class="grid gap-1">
                         <span>Observação:</span>
-                        <input type="text" name="observacao" class="dark:bg-slate-800" value="Escreva">
+                        <input type="text" name="observacao" class="dark:bg-slate-800 relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 sm:text-sm sm:leading-6" value="Escreva">
                         <x-input-error class="mt-2" :messages="$errors->get('observacao')" />
                     </div>
                 </div>
@@ -25,7 +25,7 @@
                     <div class="grid grid-cols-1 gap-2">
                         <div class="grid gap-1">
                             <span>Pesquisar:</span>
-                            <input type="text" name="search" class="dark:bg-slate-800" wire:model.prevent="search">
+                            <input type="text" name="search" class="dark:bg-slate-800 relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 sm:text-sm sm:leading-6" wire:model.prevent="search">
                             <x-input-error class="mt-2" :messages="$errors->get('search')" />
                         </div>
                     @if ($medicamentos['content'] == null)
