0ad1d9740388b54ba8159b7822282ac1013a76c9
eiidm <luisa.eid@unesp.br>
2023-09-30 08:38:08 -0300

    feat:update medico

--- resources/views/medico/forms_diario/criarForm.blade.php
+++ resources/views/medico/forms_diario/criarForm.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 gap-4">
-    <h1 class="text-xl">Criando formulário para paciente</h1>
+<div class="grid grid-cols-1 gap-4 justify-center items-center ">
+    <h1 class="text-xl font-bold text-center">Criando formulário para paciente</h1>
     @livewire('busca-nome-remedio', ['medico' => $medico, 'row' => $row])
 </div>
--- resources/views/medico/visualizacao/show.blade.php
+++ resources/views/medico/visualizacao/show.blade.php
@@ -1,9 +1,10 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
     <div class="lm-3">
-        <div class="px-4 sm:px-0">
-            <h3 class="text-lg font-bold leading-7">Suas informações</h3>
+        <div class="grid grid-cols-1 gap-4 justify-center items-center ">
+            <h3 class="text-lg font-bold leading-7 text-center">Suas informações</h3>
         </div><br><hr>
+    
       
         <br>
         <label class="font-semibold">Médico:</label> {{ Str::ucfirst($medico->nome) }}
@@ -12,16 +13,17 @@
         <br>
         <label class="font-semibold">Especialidade:</label> {{ $medico->especialidade }}
         <br><br>
-        <hr><br>
-        Links: 
-        <a href=""></a>
         
         <hr>
         <div class="px-4 sm:px-0">
-            <br><h3 class="text-lg font-bold leading-7">Parte de testes</h3><br>
+            <br><h3 class="text-lg font-bold leading-7 text-gray-600">Seção do Paciente</h3><br>
         </div>
-        <label class="font-semibold">Formulário </label> <a href="{{ route('areamedico.criarForms') }}" class="px-11 hover:font-bold hover:text-purple-800">Ver formulário</a><br>
-        <label class="font-semibold">Meus pacientes</label> <a href="{{ route('areamedico.meusPacientes') }}" class="px-3 hover:font-bold hover:text-purple-800">Ver pacientes</a>
+
+        <div class="pb-2">
+            <label class="font-semibold">Formulário </label> <a href="{{ route('areamedico.criarForms') }}" class="px-11 hover:font-bold hover:text-gray-700">Ver formulário</a><br>
+        </div>
+
+        <label class="font-semibold">Meus pacientes</label> <a href="{{ route('areamedico.meusPacientes') }}" class="px-3 hover:font-bold hover:text-gray-700">Ver pacientes</a>
     </div>
 </div>
 
