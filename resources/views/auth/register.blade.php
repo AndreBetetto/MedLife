87a4f1931b53a7f93a257c40ea6b7730bb1c2f8c
Miguel de Oliveira <migueldeoliveiracorreia@gmail.com>
2023-10-03 23:54:48 -0300

    feat: login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,23 +1,23 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" class="flex flex-col items-center justify center" action="{{ route('register') }}">
         @csrf
-
+        <h1 class="mx-1/6 font-bold text-2xl w-4/6 mb-5">Registrar</h1>
         <!-- Name -->
-        <div>
+        <div class="w-4/6">
             <x-input-label for="name" :value="__('Nome')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4">
+        <div class="mt-4 w-4/6">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        <div class="mt-4 w-4/6">
             <x-input-label for="password" :value="__('Senha')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
@@ -29,7 +29,7 @@
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
+        <div class="mt-4 w-4/6">
             <x-input-label for="password_confirmation" :value="__('Confirmar Senha')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
@@ -38,11 +38,13 @@
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
-
-        <div class="flex items-center justify-end mt-4">
+        
+        <div class="flex items-center justify-end mt-4 w-4/6">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Está registrado?') }}
             </a>
+        </div>
+        <div class="flex items-center justify-center mt-4 w-4/6">
 
             <x-primary-button class="ml-4">
                 {{ __('Registrar') }}
