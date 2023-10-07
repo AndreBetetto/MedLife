4dc9fefff026fd6a317f431eaba1ef5d03316dfd
AndreBetetto <andre.betetto@unesp.br>
2023-10-07 09:04:43 -0300

    feat:update-user-forms

--- resources/views/components/head/tinymce-config.blade.php
+++ resources/views/components/head/tinymce-config.blade.php
@@ -7,3 +7,11 @@
     save_enablewhendirty: false
   });
 </script>
+<script>
+  tinymce.init({
+    selector: 'textarea#observacoesPaciente', // Replace this CSS selector to match the placeholder element for TinyMCE
+    plugins: 'code table lists save',
+    toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table | save',
+    save_enablewhendirty: false
+  });
+</script>
