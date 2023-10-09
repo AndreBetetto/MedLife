4520424798547e3b165bd71c210f64d44a1c9c20
AndreBetetto <andre.betetto@unesp.br>
2023-10-09 16:52:14 -0300

    feat:update-meus-pacientes

--- resources/views/medico/meus-pacientes/show.blade.php
+++ resources/views/medico/meus-pacientes/show.blade.php
@@ -19,38 +19,11 @@
                 $fileCount = 1;
             }
         @endphp 
-        <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            @forelse ($pacientes as $paciente)
-                @php
-                    $pacienteId = $paciente->id;
-                    $isSelected = $pacMeds->contains('medico_id', $pacienteId);
-                    $qntForms = $formsDiario->where('medico_id', $medico->id)
-                                        ->where('paciente_id', $pacienteId)
-                                        ->count();
-                    $imgIndex = $pacienteId % $fileCount;
-                    $imgIndex = $imgIndex == 0 ? $fileCount : $imgIndex;
-                    $imgPath = 'profilePics/'.$imgIndex.'.svg';
-                @endphp
-                <div>
-                    @if (!$isSelected) 
-                        <div>  
-                            <img src="{{asset($imgPath)}}" alt="Paciente" class="h-full w-full object-cover object-center group-hover:opacity-75 rounded-full">
-                        </div>
-                        <div class="text-center text-lg">
-                            {{ $paciente->nome }} 
-                            {{ $paciente->sobrenome }} 
-                            <br>
-                            <a href="{{ route('areamedico.acessoProcessos', ['idPac' => $paciente->id]) }}" class="hover:font-semibold">Ver respostas</a>
-                            <form action="{{ route('areamedico.meusPacientescriarForm', ['id' => $paciente->id]) }}" method="GET">
-                                @csrf
-                                <button type="submit" class="font-bold">Criar formulário</button>
-                            </form>
-                        </div>
-                    @endif
-                </div>
-            @empty
-                <p>Sem pacientes adicionados</p>
-            @endforelse
+        <div>
+            @livewire('meus-pacientes', ['user' => $user, 
+                                        'medico' => $medico, 
+                                        'formsDiario' => $formsDiario,
+                                        'pacMeds' => $pacMeds])
         </div>
     </div>
 </div>
