2ffa8d8597ddd5d8bc6803e863b89e7678008293
Miguel de Oliveira Correia <ra2157051@CTIBAURU.FEB.UNESP.BR>
2023-10-17 10:50:12 -0300

    feat: remove dashboard

--- routes/web.php
+++ routes/web.php
@@ -35,9 +35,9 @@ Route::get('/aboutUs', [PagesController::class, 'aboutUs'])->name('aboutUs');
 Route::get('/sobreNos', [PagesController::class, 'sobreNos'])->name('sobreNos');
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
