b69814bcaea35bcf508476fb90d07cf5fb0684fc
AndreBetetto <andre.betetto@unesp.br>
2023-10-20 11:15:29 -0300

    feat:update

---
+++ resources/views/components/modals/medico/exam.blade.php
@@ -0,0 +1,12 @@
+<x:modals.medico.form-modal>
+
+    <div>
+        <input type="text" name="email" value=""
+    </div>
+    <input type="text" name="header">
+    
+    <div class="">
+        <x-primary-button>{{ __('Enviar') }}</x-primary-button>
+        <x-primary-button type="reset">{{ __('Limpar') }}</x-primary-button>
+    </div>
+</x:modals.medico.form-modal>
--- resources/views/livewire/meus-pacientes.blade.php
+++ resources/views/livewire/meus-pacientes.blade.php
@@ -21,7 +21,7 @@
         }
         
     @endphp 
-    <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+    <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 mt-9 ">
         @forelse ($pacientes as $index=>$paciente)
             @php
                 
@@ -55,6 +55,7 @@
                         
                         <br>
                         <a href="{{ route('areamedico.acessoProcessos', ['idPac' => $pacienteId]) }}" class="hover:font-semibold">Ver respostas</a>
+                        <x:modals.medico.exam :user='$paciente->fone'/>
                         <form action="{{ route('areamedico.meusPacientescriarForm', ['id' => $pacienteId]) }}" method="GET">
                             @csrf
                             <button type="submit" class="font-bold">Criar formulário</button>
